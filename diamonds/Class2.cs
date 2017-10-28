93121d7a5c0117932227f0d844d0fa7b6285a262
Hayakawa <Hayakawa@MARINA>
2017-10-28 18:41:59 +0600

    считка

---
+++ diamonds/Class2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace diamonds
+{
+    public class Class2
+    {
+            public void FileLoader(string path)
+            {
+            var list = new List<string[]>();
+            var lines = File.ReadAllLines(path);
+            foreach (var line in lines)
+            {
+                var cells = line.Split(',');
+                list.Add(cells);
+            }
+            return list;
+        }
+    }
+}
--- diamonds/MainWindow.xaml.cs
+++ diamonds/MainWindow.xaml.cs
@@ -26,6 +26,7 @@ namespace diamonds
             InitializeComponent();
             i = 5;
             
+            
         }
     }
 }
